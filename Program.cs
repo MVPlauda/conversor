6c440477661f1f9f872b517ee85c42d0f9ee0d5a
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:48:55 -0300

    conversor

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Converta metros para centimetros
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float metros, centimetros;
+        Console.Write("Digite os metros: ");
+        metros = float.Parse(Console.ReadLine());
+        centimetros = metros * 100;
+
+        Console.Write($"A conversão de metros para centimetros ficou: {centimetros}" );
+    }
+}
+
